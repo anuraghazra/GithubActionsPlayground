51bf857fa5192d1f8295e4a3ceffb30578b6122f
anuraghazra <hazru.anurag@gmail.com>
2019-11-08 13:25:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const Octokit = require('@octokit/rest');
+const Parser = require('markdown-parser');
+require('dotenv').config();
+
+
+// DONT DO THIS
+console.oldLog = console.log;
+console.log = function (value) {
+  console.oldLog(value);
+  return value;
+};
+
+// default configs
+let owner = 'anuraghazra';
+let repo = 'GithubActionsPlayground';
+
+// initialize contructors
+const parser = new Parser();
+const octokit = new Octokit({
+  auth: process.env.OCTOCAT_KEY
+});
+
+
+// get data
+(async () => {
+  const { data: issuesRes } = await octokit.issues.listForRepo({
+    owner,
+    repo
+  })
+
+  let issueNumber = issuesRes[0].number
+  let issueBody = issuesRes[0].body
+  // console.log(issueBody)
+
+  async function createComment(msg) {
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: `**Code executed:**\n\n\`\`\`bash\n${JSON.stringify(msg)}\n\`\`\``
+    })
+  }
+
+  // parse markdown
+  parser.parse(issueBody, function (err, result) {
+    if (err) throw new Error(err);
+    if (result.codes[0].code.includes('process.env.')) { 
+      throw new Error('Violation')
+    };
+
+    let executedCode = eval(result.codes[0].code.replace(/\n,/igm))
+    createComment(executedCode)
+  })
+})()
