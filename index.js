c77d33b8aa01929be850a4e6c052318342c2d8bb
anuraghazra <hazru.anurag@gmail.com>
2019-11-08 14:06:14 +0530

    chore: added VM

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const Octokit = require('@octokit/rest');
 const Parser = require('markdown-parser');
+const { VM } = require('vm2');
+
 require('dotenv').config();
 
+// initialize VM
+const vm = new VM({
+  timeout: 1000,
+  sandbox: {},
+  eval: false
+});
 
 // DONT DO THIS
+const consoleOverwriteScript = `
 console.oldLog = console.log;
 console.log = function (value) {
   console.oldLog(value);
   return value;
 };
+`
 
 // default configs
 let owner = 'anuraghazra';
@@ -43,12 +53,10 @@ const octokit = new Octokit({
   // parse markdown
   parser.parse(issueBody, function (err, result) {
     if (err) throw new Error(err);
-    process.env = {};
-    // if (result.codes[0].code.includes('process.env.')) { 
-    //   throw new Error('Violation')
-    // };
 
-    let executedCode = eval(result.codes[0].code.replace(/\n,/igm))
-    createComment(executedCode)
+    let code = result.codes[0].code.replace(/\n,/igm)
+    let res = vm.run(`${consoleOverwriteScript}\n${code}`)
+
+    createComment(res)
   })
 })()
